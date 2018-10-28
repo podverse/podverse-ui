335536fbb6dc87e7a240fe5eba3393735ffd0338
Mitch Downey <mitchldowney@gmail.com>
2018-10-27 21:57:55 -0500

    Start to add Queue modal; adjust media player button heights

--- src/components/Media/MediaPlayer/QueueModal/QueueModal.tsx
+++ src/components/Media/MediaPlayer/QueueModal/QueueModal.tsx
@@ -6,13 +6,30 @@ export interface Props {
   isOpen: boolean
 }
 
+const customStyles = {
+  content: {
+    bottom: 'unset',
+    left: '50%',
+    maxWidth: '420px',
+    overflow: 'unset',
+    right: 'unset',
+    top: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '100%'
+  }
+}
+
 export const QueueModal: React.StatelessComponent<Props> = props => {
   const { hideModal, isOpen } = props
 
   return (
     <Modal
       contentLabel='Queue'
-      isOpen={isOpen}>
+      isOpen={isOpen}
+      onRequestClose={hideModal}
+      portalClassName='mp-queue-modal over-media-player'
+      shouldCloseOnOverlayClick
+      style={customStyles}>
       Queue modal :)
       <button onClick={hideModal}>
         Hide modal
