d3e4ed74e3c3b69a94a1ee8f6774ce42344c8547
Mitch Downey <mitchldowney@gmail.com>
2018-11-18 02:10:49 -0600

    Add FontAwesome spinner icon

--- src/lib/fontAwesomeIcons.ts
+++ src/lib/fontAwesomeIcons.ts
@@ -1,8 +1,8 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCut, faEllipsisH, faGlobeAmericas, faInfinity, faLevelDownAlt,
   faLevelUpAlt, faLink, faListUl, faPause, faPlay, faPlayCircle, faPlus, faPlusCircle,
-  faRedoAlt, faSearch, faShare, faStepBackward, faStepForward, faTimes, faUndoAlt,
-  faVolumeOff, faVolumeUp } from '@fortawesome/free-solid-svg-icons'
+  faRedoAlt, faSearch, faShare, faSpinner, faStepBackward, faStepForward, faTimes,
+  faUndoAlt, faVolumeOff, faVolumeUp } from '@fortawesome/free-solid-svg-icons'
 
 // Adds FontAwesome icons to the library so they can be used throughout the app
 export const addFontAwesomeIcons = () => {
@@ -23,6 +23,7 @@ export const addFontAwesomeIcons = () => {
     faRedoAlt,
     faSearch,
     faShare,
+    faSpinner,
     faStepBackward,
     faStepForward,
     faTimes,
