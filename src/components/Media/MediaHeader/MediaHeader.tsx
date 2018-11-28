fa1ddf41725065d98740b91fc542b0b58432e9ae
Mitch Downey <mitchldowney@gmail.com>
2018-11-27 20:36:11 -0600

    Enable subscribing to podcasts

--- src/components/Media/MediaHeader/MediaHeader.tsx
+++ src/components/Media/MediaHeader/MediaHeader.tsx
@@ -1,16 +1,24 @@
 import * as React from 'react'
 import { getEpisodeUrl, getPodcastUrl } from 'lib/constants'
 import { readableDate } from 'lib/utility'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons'
+import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons'
 
 type Props = {
   episode?: any
+  handleToggleSubscribe?: any
+  isSubscribed?: boolean
+  isSubscribing?: boolean
   mediaRef?: any
   nowPlayingItem?: any
   podcast?: any
+  subscribedIds?: any[]
 }
 
 export const MediaHeader: React.StatelessComponent<Props> = props => {
-  const { episode, mediaRef, nowPlayingItem, podcast } = props
+  const { episode, handleToggleSubscribe, isSubscribed, isSubscribing, mediaRef,
+    nowPlayingItem, podcast } = props
 
   let bottomText
   let bottomTextSide
@@ -72,6 +80,23 @@ export const MediaHeader: React.StatelessComponent<Props> = props => {
     <div className='media-header'>
       <img className='media-header__image' src={imgUrl} />
       <div className='text-wrapper'>
+        <button
+          className='media-header__subscribe'
+          onClick={handleToggleSubscribe}>
+          {
+            isSubscribing ?
+              <FontAwesomeIcon icon='spinner' spin />
+              :
+              <React.Fragment>
+                {
+                  isSubscribed ?
+                    <FontAwesomeIcon icon={fasStar} />
+                    // @ts-ignore
+                    : <FontAwesomeIcon icon={farStar} />
+                }
+              </React.Fragment>
+          }
+        </button>
         {
           title &&
             <a className='media-header__title' href={titleLink}>{title}</a>
