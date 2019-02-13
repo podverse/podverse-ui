baf36faeada86ed518660fb0fc6e3fb4cdc5f57a
Mitch Downey <mitchldowney@gmail.com>
2019-02-12 23:16:16 -0600

    Update fontAwesomeIcons.ts

--- src/lib/fontAwesomeIcons.ts
+++ src/lib/fontAwesomeIcons.ts
@@ -1,10 +1,10 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCaretDown, faCaretRight, faCheck, faCut, faDownload, faEdit, faEllipsisH,
-  faGlobeAmericas, faHeadphones, faHistory, faInfinity, faLevelDownAlt, faLevelUpAlt,
-  faLink, faListUl, faLock, faMoon, faPause, faPlay, faPlayCircle, faPlus, faPlusCircle,
-  faRedoAlt, faSearch, faShare, faShoppingCart, faSmile, faSpinner, faStepBackward,
-  faStepForward, faSun, faTimes, faTrash, faUndoAlt, faUserCircle, faUserSecret,
-  faVolumeOff, faVolumeUp } from '@fortawesome/free-solid-svg-icons'
+  faFilter, faGlobeAmericas, faHeadphones, faHistory, faInfinity, faLevelDownAlt,
+  faLevelUpAlt, faLink, faListUl, faLock, faMoon, faPause, faPlay, faPlayCircle, faPlus,
+  faPlusCircle, faRedoAlt, faSearch, faShare, faShoppingCart, faSmile, faSpinner,
+  faStepBackward, faStepForward, faSun, faTimes, faTrash, faUndoAlt, faUserCircle,
+  faUserSecret, faVolumeOff, faVolumeUp } from '@fortawesome/free-solid-svg-icons'
 
 // Adds FontAwesome icons to the library so they can be used throughout the app
 export const addFontAwesomeIcons = () => {
@@ -16,6 +16,7 @@ export const addFontAwesomeIcons = () => {
     faDownload,
     faEdit,
     faEllipsisH,
+    faFilter,
     faGlobeAmericas,
     faHeadphones,
     faHistory,
