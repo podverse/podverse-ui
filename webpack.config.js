4a98303a66b15c598a8b34275ae1e60f12d390c2
Mitch Downey <mitchldowney@gmail.com>
2019-12-07 20:55:42 -0600

    Add .eslintignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const { resolve } = require('path')
-const webpack = require('webpack')
 const nodeExternals = require('webpack-node-externals')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
